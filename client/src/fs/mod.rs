cd37a500d11884685e33aa2a877b6f99e219d6fa
Beccio00 <riccardo.becciolini00@gmail.com>
2025-09-16 13:20:07 +0200

    fix(duseAdapter): implement all the methods of FuseAdapter and add the async-trait

--- client/src/fs/mod.rs
+++ client/src/fs/mod.rs
@@ -1,9 +1,18 @@
 // Common trait between different OS implementations
+use async_trait::async_trait;
 
 pub mod linux;
 pub mod macos;
 pub mod windows;
 
+#[async_trait]
 pub trait FuseAdapter {
-    fn mount(&self, mountpoint: &str) -> Result<(), String>;
+    fn init() -> Result<Self, String> where Self: Sized;
+    
+    async fn mount(&self, mountpoint: &str) -> Result<(), String>;
+
+    async fn unmount(&self, mountpoint: &str) -> Result<(), String>;
+
+    fn is_mounted(&self, mountpoint: &str) -> Result<bool, String>;
+
 }
