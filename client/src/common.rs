4d408d006805d734308c9484f22321c55d9d64fe
giova95 <angeligiovanni21@gmail.com>
2025-09-27 18:32:42 +0200

    feat(client): add rename, create, write dir and files

--- client/src/common.rs
+++ client/src/common.rs
@@ -258,4 +258,281 @@ impl Filesystem for RemoteFS {
             reply.error(libc::ENOENT);
         }
     }
+
+    fn create(
+        &mut self,
+        _req: &Request<'_>,
+        parent: u64,
+        name: &OsStr,
+        _mode: u32,
+        _umask: u32,
+        _flags: i32,
+        reply: fuser::ReplyCreate,
+    ) {
+        let i2p = self.inode_to_path.lock().unwrap();
+        let parent_path = i2p.get(&parent).cloned().unwrap_or_default();
+        drop(i2p);
+
+        let full_path = if parent_path.is_empty() {
+            name.to_string_lossy().to_string()
+        } else {
+            format!("{}/{}", parent_path, name.to_string_lossy())
+        };
+
+        // Create empty file on server
+        let url = format!("{}/files/{}", self.base_url, full_path);
+        match self.client.put(&url).body("").send() {
+            Ok(resp) if resp.status().is_success() => {
+                let ino = self.alloc_inode(full_path);
+                let ttl = Duration::from_secs(1);
+                let attr = FileAttr {
+                    ino,
+                    size: 0,
+                    blocks: 0,
+                    atime: SystemTime::now(),
+                    mtime: SystemTime::now(),
+                    ctime: SystemTime::now(),
+                    crtime: SystemTime::now(),
+                    kind: FileType::RegularFile,
+                    perm: 0o644,
+                    nlink: 1,
+                    uid: 1000,
+                    gid: 1000,
+                    rdev: 0,
+                    blksize: 512,
+                    flags: 0,
+                };
+                reply.created(&ttl, &attr, 0, ino, 0);
+            }
+            _ => reply.error(libc::EIO),
+        }
+    }
+
+    fn write(
+        &mut self,
+        _req: &Request<'_>,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        data: &[u8],
+        _write_flags: u32,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        reply: fuser::ReplyWrite,
+    ) {
+        let i2p = self.inode_to_path.lock().unwrap();
+        let path = i2p.get(&ino).cloned();
+        drop(i2p);
+
+        if let Some(file_path) = path {
+            // For simplicity, we'll do full file replacement for now
+            // In a real implementation, you'd want to handle partial writes
+            if offset == 0 {
+                let url = format!("{}/files/{}", self.base_url, file_path);
+                match self.client.put(&url).body(data.to_vec()).send() {
+                    Ok(resp) if resp.status().is_success() => {
+                        reply.written(data.len() as u32);
+                    }
+                    _ => reply.error(libc::EIO),
+                }
+            } else {
+                // For offset writes, we'd need to read, modify, write
+                // This is a simplified implementation
+                reply.error(libc::ENOSYS);
+            }
+        } else {
+            reply.error(libc::ENOENT);
+        }
+    }
+
+    fn mkdir(
+        &mut self,
+        _req: &Request<'_>,
+        parent: u64,
+        name: &OsStr,
+        _mode: u32,
+        _umask: u32,
+        reply: ReplyEntry,
+    ) {
+        let i2p = self.inode_to_path.lock().unwrap();
+        let parent_path = i2p.get(&parent).cloned().unwrap_or_default();
+        drop(i2p);
+
+        let full_path = if parent_path.is_empty() {
+            name.to_string_lossy().to_string()
+        } else {
+            format!("{}/{}", parent_path, name.to_string_lossy())
+        };
+
+        let url = format!("{}/mkdir/{}", self.base_url, full_path);
+        match self.client.post(&url).send() {
+            Ok(resp) if resp.status().is_success() => {
+                let ino = self.alloc_inode(full_path);
+                let ttl = Duration::from_secs(1);
+                let attr = FileAttr {
+                    ino,
+                    size: 0,
+                    blocks: 0,
+                    atime: SystemTime::now(),
+                    mtime: SystemTime::now(),
+                    ctime: SystemTime::now(),
+                    crtime: SystemTime::now(),
+                    kind: FileType::Directory,
+                    perm: 0o755,
+                    nlink: 2,
+                    uid: 1000,
+                    gid: 1000,
+                    rdev: 0,
+                    blksize: 512,
+                    flags: 0,
+                };
+                reply.entry(&ttl, &attr, 0);
+            }
+            _ => reply.error(libc::EIO),
+        }
+    }
+
+    fn unlink(&mut self, _req: &Request<'_>, parent: u64, name: &OsStr, reply: fuser::ReplyEmpty) {
+        let i2p = self.inode_to_path.lock().unwrap();
+        let parent_path = i2p.get(&parent).cloned().unwrap_or_default();
+        drop(i2p);
+
+        let full_path = if parent_path.is_empty() {
+            name.to_string_lossy().to_string()
+        } else {
+            format!("{}/{}", parent_path, name.to_string_lossy())
+        };
+
+        let url = format!("{}/files/{}", self.base_url, full_path);
+        match self.client.delete(&url).send() {
+            Ok(resp) if resp.status().is_success() => {
+                // Remove from our cache
+                let mut p2i = self.path_to_inode.lock().unwrap();
+                if let Some(ino) = p2i.remove(&full_path) {
+                    self.inode_to_path.lock().unwrap().remove(&ino);
+                }
+                reply.ok();
+            }
+            _ => reply.error(libc::EIO),
+        }
+    }
+
+    fn rmdir(&mut self, _req: &Request<'_>, parent: u64, name: &OsStr, reply: fuser::ReplyEmpty) {
+        // Same as unlink for our simple implementation
+        self.unlink(_req, parent, name, reply);
+    }
+
+    fn rename(
+        &mut self,
+        _req: &Request<'_>,
+        parent: u64,
+        name: &OsStr,
+        newparent: u64,
+        newname: &OsStr,
+        _flags: u32,
+        reply: fuser::ReplyEmpty,
+    ) {
+        let i2p = self.inode_to_path.lock().unwrap();
+        let parent_path = i2p.get(&parent).cloned().unwrap_or_default();
+        let newparent_path = i2p.get(&newparent).cloned().unwrap_or_default();
+        drop(i2p);
+
+        let old_path = if parent_path.is_empty() {
+            name.to_string_lossy().to_string()
+        } else {
+            format!("{}/{}", parent_path, name.to_string_lossy())
+        };
+
+        let new_path = if newparent_path.is_empty() {
+            newname.to_string_lossy().to_string()
+        } else {
+            format!("{}/{}", newparent_path, newname.to_string_lossy())
+        };
+
+        // Simple implementation: read old file, write new file, delete old
+        match self.read_file(&old_path) {
+            Ok(data) => {
+                let write_url = format!("{}/files/{}", self.base_url, new_path);
+                let delete_url = format!("{}/files/{}", self.base_url, old_path);
+
+                if let Ok(resp) = self.client.put(&write_url).body(data).send() {
+                    if resp.status().is_success() {
+                        if let Ok(resp) = self.client.delete(&delete_url).send() {
+                            if resp.status().is_success() {
+                                // Update our cache
+                                let mut p2i = self.path_to_inode.lock().unwrap();
+                                if let Some(ino) = p2i.remove(&old_path) {
+                                    p2i.insert(new_path.clone(), ino);
+                                    self.inode_to_path.lock().unwrap().insert(ino, new_path);
+                                }
+                                reply.ok();
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+        reply.error(libc::EIO);
+    }
+
+    fn setattr(
+        &mut self,
+        _req: &Request<'_>,
+        ino: u64,
+        _mode: Option<u32>,
+        _uid: Option<u32>,
+        _gid: Option<u32>,
+        size: Option<u64>,
+        _atime: Option<fuser::TimeOrNow>,
+        _mtime: Option<fuser::TimeOrNow>,
+        _ctime: Option<SystemTime>,
+        _fh: Option<u64>,
+        _crtime: Option<SystemTime>,
+        _chgtime: Option<SystemTime>,
+        _bkuptime: Option<SystemTime>,
+        _flags: Option<u32>,
+        reply: ReplyAttr,
+    ) {
+        // Handle file truncation
+        if let Some(new_size) = size {
+            let i2p = self.inode_to_path.lock().unwrap();
+            if let Some(path) = i2p.get(&ino).cloned() {
+                drop(i2p);
+
+                if new_size == 0 {
+                    // Truncate to zero
+                    let url = format!("{}/files/{}", self.base_url, path);
+                    if let Ok(resp) = self.client.put(&url).body("").send() {
+                        if resp.status().is_success() {
+                            let ttl = Duration::from_secs(1);
+                            let attr = FileAttr {
+                                ino,
+                                size: 0,
+                                blocks: 0,
+                                atime: SystemTime::now(),
+                                mtime: SystemTime::now(),
+                                ctime: SystemTime::now(),
+                                crtime: SystemTime::now(),
+                                kind: FileType::RegularFile,
+                                perm: 0o644,
+                                nlink: 1,
+                                uid: 1000,
+                                gid: 1000,
+                                rdev: 0,
+                                blksize: 512,
+                                flags: 0,
+                            };
+                            reply.attr(&ttl, &attr);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        // For other attributes, just return current attributes
+        self.getattr(_req, ino, reply);
+    }
 }
